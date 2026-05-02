bbe94af383b58104b5c8ddbfd6c742a065954e65
Hegyi Áron Ferenc <36823200+Rettend@users.noreply.github.com>
2026-05-02 17:53:44 +0200

    feat: draft create alias

--- apps/bot/src/commands/match/index.ts
+++ apps/bot/src/commands/match/index.ts
@@ -1,2 +1,2 @@
-export { command_match } from './command.ts'
+export { command_draft, command_match } from './command.ts'
 export { component_draft_activity, component_match_browse, component_match_join } from './components.ts'
